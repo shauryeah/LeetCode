6136759eca15196107a77185e8ba23e431ad39ec
shauryeah <shaurya14.sharma@gmail.com>
2025-01-27 14:20:34 +0530

    Time: 5707 ms (5.00%) | Memory: 13.5 MB (12.31%) - LeetSync

---
+++ 1-two-sum/two-sum.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i!=j:
+                    if nums[i]+nums[j]==target:
+                        A=[i,j]
+                        return A
+                    else:
+                        continue
+                else:
+                    continue
+        return 0
