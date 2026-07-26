95de7daf11e4737600564e68ee0a5f5318212303
Shaurya Sharma <shaurya14.sharma@gmail.com>
2026-07-26 20:45:09 +0530

    Time: 0 ms (100%), Space: 12.7 MB (24.02%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def nextGreaterElement(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        num1={}
+        stack=[]
+        ans=[]
+        for j in nums2[::-1]:
+            while(stack and stack[-1]<j):
+                stack.pop()
+            if(stack):
+                num1[j]=stack[-1]
+                stack.append(j)
+            else:
+                num1[j]=-1
+                stack.append(j)
+        for i in nums1:
+            ans.append(num1[i])
+        return ans
+
+
+
+
+
+            
