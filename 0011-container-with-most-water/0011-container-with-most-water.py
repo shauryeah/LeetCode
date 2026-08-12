9fde90275d961fc10fab4806d8811b1aa594c10a
Shaurya Sharma <shaurya14.sharma@gmail.com>
2026-08-12 17:31:06 +0530

    Time: 122 ms (36.61%), Space: 20.8 MB (24.61%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.py
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -6,13 +6,13 @@ class Solution(object):
         """
         left=0
         right=len(height)-1
-        area=0
+        Area=0
         while(left<right):
-            area=max(min(height[left],height[right])*(right-left),area)
+            Area=max(Area,min(height[left],height[right])*(right-left))
             if(height[left]<height[right]):
                 left+=1
             else:
                 right-=1
-        return area
+        return Area
 
 
