63e62d6978d03c3c84d0fd70425e38deb6626cde
Shaurya Sharma <shaurya14.sharma@gmail.com>
2026-08-12 22:00:44 +0530

    Time: 765 ms (44.99%), Space: 18.9 MB (25.48%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -5,26 +5,26 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        sol=[]
+        ans=[]
         for i in range(len(nums)-2):
             if i!=0 and nums[i-1]==nums[i]:
                 continue
             left=i+1
             right=len(nums)-1
             while(left<right):
-                target=nums[left]+nums[right]+nums[i]
+                target=nums[i]+nums[left]+nums[right]
                 if(target==0):
-                    sol.append([nums[i],nums[left],nums[right]])
+                    ans.append([nums[i],nums[left],nums[right]])
                     left+=1
                     right-=1
-                    while((left<right) and (nums[left]==nums[left-1])):
+                    while(left<right and nums[left]==nums[left-1]):
                         left+=1
-                    while((left<right) and (nums[right]==nums[right+1])):
+                    while(left<right and nums[right]==nums[right+1]):
                         right-=1
                 elif(target<0):
                     left+=1
                 else:
                     right-=1
-        return sol
+        return ans
 
                 
